cc726ab5363e63bd1902653e541db60a6a8bbdbb
RonLópez <ron@air-de-ronlopez.home>
2024-07-25 09:45:19 -0300

    wait groups

--- gorutineI.go
+++ gorutineI.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+//Paquete sync
+// Aplicando WaitGroups()
+
+var wg sync.WaitGroup
 
 func main() {
 
 	fmt.Println("Esto es una gorutine")
-	foo()
+	runtimePack()
+
+	wg.Add(1)
+	go foo()
+	bar()
+	wg.Wait()
 
 }
 
@@ -14,4 +28,21 @@ func foo() {
 	for i := 0; i <= 10; i++ {
 		fmt.Println("foo()", i)
 	}
+	wg.Done()
+}
+
+func bar() {
+
+	for j := 0; j <= 10; j++ {
+
+		fmt.Println("bar()", j)
+	}
+}
+func runtimePack() {
+	//Paquete runtime
+	fmt.Println("OS", runtime.GOOS)                   //Sistema Operativo
+	fmt.Println("ARCH", runtime.GOARCH)               //Arquitectura
+	fmt.Println("CPU", runtime.NumCPU())              //Numero de CPU
+	fmt.Println("#Gorutines", runtime.NumGoroutine()) //Numero de Gorutines
+
 }
